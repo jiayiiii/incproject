3ca257b6c17bdb3f5a3ddb2a5ee66f32b63ad8fc
jiayi <yipjiayi11@gmail.com>
2024-09-09 17:23:24 +0800

    Initial Commit

---
+++ incproject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  incproject
+//
+//  Created by jia yi on 9/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ incproject/incprojectApp.swift
@@ -0,0 +1,17 @@
+//
+//  incprojectApp.swift
+//  incproject
+//
+//  Created by jia yi on 9/9/24.
+//
+
+import SwiftUI
+
+@main
+struct incprojectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
