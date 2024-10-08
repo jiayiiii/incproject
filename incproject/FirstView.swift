1a9b381363f4c38cf30fc598b75c56d05305cc6d
jiayi <yipjiayi11@gmail.com>
2024-10-08 15:34:45 +0800

    added firstview (first minigame to uncover his accomplice) made contentview navigate to firstview

---
+++ incproject/FirstView.swift
@@ -0,0 +1,18 @@
+//
+//  FirstView.swift
+//  incproject
+//
+//  Created by jia yi on 8/10/24.
+//
+
+import SwiftUI
+
+struct FirstView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FirstView()
+}
