124e861ea6ee9cbbc56a6a36ccb32c1d56f9d54b
Sharlene Tan Qin Ying <sharlene_tan_qin_ying@s2024.ssts.edu.sg>
2024-10-09 12:53:38 +0800

    Added the puzzle

---
+++ HackerGameView.swift
@@ -0,0 +1,18 @@
+//
+//  HackerGameView.swift
+//  incproject
+//
+//  Created by Sharlene Tan Qin Ying on 9/10/24.
+//
+
+import SwiftUI
+
+struct HackerGameView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HackerGameView()
+}
