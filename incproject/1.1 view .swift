01a51f01ae1273643a46b458cfb54277e0794514
Sharlene Tan Qin Ying <sharlene_tan_qin_ying@s2024.ssts.edu.sg>
2024-10-09 12:12:46 +0800

    ...

---
+++ incproject/1.1 view .swift
@@ -0,0 +1,18 @@
+//
+//  1.1 view .swift
+//  incproject
+//
+//  Created by Sharlene Tan Qin Ying on 9/10/24.
+//
+
+import SwiftUI
+
+struct __1_view_: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    __1_view_()
+}
