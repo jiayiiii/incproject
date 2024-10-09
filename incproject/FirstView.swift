41d08ac6efdc8a5d8825e9275010163e3357b3ec
tanxintongjoy <tan_xin_tong_joy@s2024.ssts.edu.sg>
2024-10-09 11:52:27 +0800

    Update FirstView.swift
    added like some stuff to first view

--- incproject/FirstView.swift
+++ incproject/FirstView.swift
@@ -10,8 +10,14 @@ import SwiftUI
 struct FirstView: View {
     var body: some View {
         NavigationStack {
+               Text("Your goal is to find out")
+                .fontWeight(.bold)
+                .fontDesign(.serif)
             Text("Who was tall avyan's accomplice?")
                 .fontWeight(.bold)
+                  .foregroundColor(.red)
+                .font(.title2)
+                .fontDesign(.serif)
         }
         .navigationTitle("First Minigame")
         .navigationBarTitleDisplayMode(.large)
