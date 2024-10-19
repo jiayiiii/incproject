c2130c157e7c330474b17b4c74fbd459aa62f804
Nadra <nadra_l>
2024-10-19 13:39:09 +0800

    just added a background and a button to continue

---
+++ HackerGameView3yep.swift
@@ -0,0 +1,18 @@
+//
+//  HackerGameView3yep.swift
+//  incproject
+//
+//  Created by Nadra binte Muhammad Shahril on 19/10/24.
+//
+
+import SwiftUI
+
+struct HackerGameView3yep: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HackerGameView3yep()
+}
