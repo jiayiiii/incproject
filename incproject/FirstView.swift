ac4e8c67340a1e3615ac561386f80ed8f2132230
joy <tan_xin_tong_joy@s2024.ssts.edu.sg>
2024-10-19 20:02:30 +0800

    changed the navigationlink

--- incproject/FirstView.swift
+++ incproject/FirstView.swift
@@ -67,7 +67,7 @@ struct FirstView: View {
                     // Navigation and image for visual impact
                     HStack(spacing: 15) {
                         NavigationLink {
-                            HackerGameView()
+                            HomeView()
                         } label: {
                             Text("Next")
                                 .font(.system(size: 22, weight: .bold))
