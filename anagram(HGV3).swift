ae1adb59f5dd2f3bc3444f783231f8302af7b91b
Nadra <nadra_l>
2024-10-19 16:23:33 +0800

    oh uh i think i did ir wrong before

---
+++ anagram(HGV3).swift
@@ -0,0 +1,18 @@
+//
+//  anagram(HGV3).swift
+//  incproject
+//
+//  Created by Nadra binte Muhammad Shahril on 19/10/24.
+//
+
+import SwiftUI
+
+struct anagram_HGV3_: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    anagram_HGV3_()
+}
