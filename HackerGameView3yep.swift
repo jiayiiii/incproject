136378ac154a3fd5210cb8dcf28a3414038af60c
Nadra <nadra_l>
2024-10-20 10:35:13 +0800

    ok im so sry 😭 but like i hope this is HackerGameView3yep PROPERLY commited & pushed 😭

--- HackerGameView3yep.swift
+++ HackerGameView3yep.swift
@@ -2,15 +2,45 @@
 //  HackerGameView3yep.swift
 //  incproject
 //
-//  Created by Nadra binte Muhammad Shahril on 19/10/24.
+//  Created by Nadra binte Muhammad Shahril on 20/10/24.
 //
 
 import SwiftUI
 
 struct HackerGameView3yep: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+        ZStack{
+            LinearGradient(gradient: Gradient(colors: [.black, .black.opacity(0.8)]),
+                           startPoint: .top, endPoint: .bottom)
+            .edgesIgnoringSafeArea(.all)
+
+            VStack{
+                Image("freakbob")
+                    .resizable()
+                    .frame(width: 500,height: 600)
+                HStack{
+                    NavigationLink(destination: anagram()) {
+                        Text("next")
+                            .padding()
+                            .background(Color.blue.opacity(0.9))
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                            .shadow(radius: 5)
+                            .padding(.top, 20)
+                    }
+                    Text("pick up?☎️")
+                        .padding()
+                        .background(Color.green.opacity(0.9))
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                        .shadow(radius: 5)
+                        .padding(.top, 20)
+                    //can someone make that when u click this, 'threatbob'(in assets), starts playing if possible if not its ok
+                }
+            }
+        }
+        }
+
 }
 
 #Preview {
---
+++ anagram.swift
@@ -0,0 +1,18 @@
+//
+//  anagram.swift
+//  incproject
+//
+//  Created by Nadra binte Muhammad Shahril on 20/10/24.
+//
+
+import SwiftUI
+
+struct anagram: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    anagram()
+}
