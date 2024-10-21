caa418dd9c537b40433847738bc2d3a67aeaf8c3
joy <tan_xin_tong_joy@s2024.ssts.edu.sg>
2024-10-21 08:31:34 +0800

    shift it up

--- HackerGameView3.swift
+++ HackerGameView3.swift
@@ -34,7 +34,7 @@ struct HackerGameView4: View {
                         .background(Color.red)
                         .cornerRadius(10)
                         .padding(.bottom, 30)
-                        .position(x: 90, y: 250)
+                        .position(x: 90, y: 200)
                     }
 
                     Button(action: {
@@ -53,7 +53,7 @@ struct HackerGameView4: View {
                         .background(Color.green)
                         .cornerRadius(10)
                         .padding(.bottom, 30)
-                        .position(x:70, y: 250)
+                        .position(x:70, y: 200)
                     }
                     .frame(maxWidth: .infinity, alignment: .trailing)
                 }
